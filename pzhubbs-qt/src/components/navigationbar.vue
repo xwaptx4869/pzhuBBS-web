def904aba12d87c078a7308b1c24a80f7f2f8a63
xuwei <is_xuwei@163.com>
2019-04-10 18:31:06 +0800

    相关

--- pzhubbs-qt/src/components/navigationbar.vue
+++ pzhubbs-qt/src/components/navigationbar.vue
@@ -1,5 +1,13 @@
 <template>
-    
+<div>
+    <el-container>
+        <el-form>
+            <el-form-item>
+                <el-input  placeholder=""></el-input>
+            </el-form-item>
+        </el-form>
+    </el-container>
+    </div>
 </template>
 
 <script>
---
+++ pzhubbs-qt/src/plugins/element.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import Element from 'element-ui'
+import '@/assets/style/element-variables.scss'
+
+Vue.use(Element)
