322ed5480eff22d91ba8a29923cbfef3070674af
xuwei <1183927842@qq.com>
2019-04-24 23:43:56 +0800

    评论、文章

---
+++ pzhubbs-qt/src/components/CommentManage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="comment-page">
+        <div class="comments-content">
+            <div class="comments-num"></div>
+            <div class="comments-list"></div>
+        </div>
+        <div class="release-box">
+            <div class="nav"></div>
+            <div class="comments-release-box">
+                <el-form :model="form" ref="form" label-width="80px">
+                    <el-form-item label="评论">
+                        <el-input  type="textarea" v-model="form.content" rows="4"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" plain >发表评论</el-button>
+                    </el-form-item>
+                </el-form>
+                
+            </div>
+        </div>
+    </div>    
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
--- pzhubbs-qt/src/components/Home.vue
+++ pzhubbs-qt/src/components/Home.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="page">
-    <div class="title">好梦向来易醒。</div>
+    <div class="title">
+      <h1>{{user.name}}</h1>
+      <span>{{user.mood}}</span> </div>
     <div class="article-box">
       <div class="article" v-for="item in articles " :key="item.id">
+        <div v-if="item.imgSrc" class="img-box" :style="{ backgroundImage:'url('+ item.imgSrc+')' }" >
+        </div>
         <h2>{{item.title}}</h2>
         <p>{{item.introduction}}</p>
         <div class="article-msg">
@@ -20,9 +24,14 @@
 export default {
   data() {
     return {
+      user:{
+        name:'许如清',
+        mood:'好梦向来易醒。'
+      },
       articles: [
         {
           title: "关于web语义化、自定义命名的规范",
+          imgSrc:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1556126003099&di=9eb8f28cd5ba53c554611499dcf5ebbb&imgtype=0&src=http%3A%2F%2Fpic.rmb.bdstatic.com%2Fd487c4c496b952e7463a9ac2a041643a.jpeg',
           introduction:
             "语义化是前端开发里面的一个专用术语，其优点在于标签语义化有助于构架良好的html结构，有利于搜索引擎的建立索引、抓取；另外，亦有利于页面在不同的设备上显示尽可能相同；此外，亦有利于构建清晰的机构，有利于团队的开发、维护",
           username: "xuwei",
@@ -39,6 +48,7 @@ export default {
         },
         {
           title: "关于web语义化、自定义命名的规范",
+          imgSrc:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1556127019172&di=057a0c7d53e9d5ad88900adbf27d2738&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201504%2F25%2F20150425H2249_aULWv.jpeg',
           introduction:
             "语义化是前端开发里面的一个专用术语，其优点在于标签语义化有助于构架良好的html结构，有利于搜索引擎的建立索引、抓取；另外，亦有利于页面在不同的设备上显示尽可能相同；此外，亦有利于构建清晰的机构，有利于团队的开发、维护",
           username: "xuwei",
@@ -80,12 +90,17 @@ export default {
    box-sizing: border-box;
   .title {
     width: 100%;
-    height: 80px;
-    line-height: 80px;
     border-bottom: 1px solid #dee5e7;
     background-color: #f9f9f9;
-    padding-left: 30px;
+    padding:30px;
     box-sizing: border-box;
+    h1{
+      font-size: 24px;
+      font-weight: 300;
+    }
+    span{
+      color: #a0a0a0;
+    }
   }
   .article-box {
     width: 100%;
@@ -96,6 +111,23 @@ export default {
       background: #fff;
       padding: 30px;
       margin-bottom: 30px;
+      .img-box{
+        width: 100%;
+        min-height: 350px;
+        cursor: pointer;
+        min-height: 350px;
+        position: relative;
+        display: block;
+        background-position: 50% 50%;
+        background-size: cover;
+        border-top-left-radius: 4px;
+        border-top-right-radius: 4px;
+        transition: all 0.6s;
+        margin-bottom: 30px;
+        &:hover{
+          transform: scale(1.08);
+        }
+      }
       h2 {
         font-size: 22px;
         margin-bottom: 10px;
